50912ba63cbfd6603ff0f21a4445414ca18bc1b2
nicat19 <nicat.balayev.01@gmail.com>
2018-02-23 20:31:12 +0400

    console

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" a ededini daxil edin");
+            var a = Console.ReadLine();
+            Console.WriteLine(" b ededini daxil edin");
+            var b = Console.ReadLine();
+            int c = Convert.ToInt32(a) + Convert.ToInt32(b);
+            Console.WriteLine(a + "+" + b + "=" + c);
+
+
+        }
+    }
+}
